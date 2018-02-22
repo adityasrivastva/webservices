b39da91e7b463745b65caa09874df3a4a614ddab
adityasrivastva <aditya5srivastva@gmail.com>
2018-02-22 21:31:07 +0530

    Added getter setter methods

--- rest-apiframework/src/main/java/com/api/rest/api/helper/model/ResponseBody.java
+++ rest-apiframework/src/main/java/com/api/rest/api/helper/model/ResponseBody.java
@@ -10,6 +10,33 @@ public class ResponseBody {
 	public String Id;
 	public String LaptopName;
 	public Features Features;
+	
+	public String getBrandName() {
+		return BrandName;
+	}
+	public void setBrandName(String brandName) {
+		BrandName = brandName;
+	}
+	public String getId() {
+		return Id;
+	}
+	public void setId(String id) {
+		Id = id;
+	}
+	public String getLaptopName() {
+		return LaptopName;
+	}
+	public void setLaptopName(String laptopName) {
+		LaptopName = laptopName;
+	}
+	public Features getFeatures() {
+		return Features;
+	}
+	public void setFeatures(Features features) {
+		Features = features;
+	}
+	
+	
 
 }
 
