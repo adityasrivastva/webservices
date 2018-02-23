1124998e035f060b0aeccff8d72cb0328759c54d
adityasrivastva <aditya5srivastva@gmail.com>
2018-02-23 20:27:28 +0530

    RequestStatus class implements FutureCallback

---
+++ rest-apiframework/src/main/java/com/api/rest/api/helper/model/RequestStatus.java
@@ -0,0 +1,24 @@
+package com.api.rest.api.helper.model;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.concurrent.FutureCallback;
+
+public class RequestStatus implements FutureCallback<HttpResponse> {
+
+	@Override
+	public void cancelled() {
+		System.out.println("---Request cancelled---");
+	}
+
+	@Override
+	public void completed(HttpResponse result) {
+		System.out.println("---Request completed---"+ result.getProtocolVersion());
+		
+	}
+
+	@Override
+	public void failed(Exception ex) {
+		System.out.println("---Request failed---"+ ex.getMessage());
+	}
+
+}
